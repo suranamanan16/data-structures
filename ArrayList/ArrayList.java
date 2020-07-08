93ed9e534fa49603d76db27b2b31c5b13ff85639
Manan Surana <59257437+suranamanan@users.noreply.github.com>
2020-07-08 15:17:16 -0400

    constructor completed

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -1,11 +1,14 @@
 //List<Integer> = ArrayList<Integer>();
-//The <E> represents the type of array we are making 
+//The <E> represents the type of array we are making
 class ArrayList <E>{
 
+  private E[] objects;
+  private int size;
 
 
   private ArrayList(){
-
+    this.objects = (E[]) new Objects[];
+    this.size = 0
   }
 
   public static void main(String[] args){
