ad4e5993914ed8e27f4c035aa37f897a35d34644
Manan Surana <59257437+suranamanan@users.noreply.github.com>
2020-07-08 15:24:23 -0400

    comments

--- ArrayList/ArrayList.java
+++ ArrayList/ArrayList.java
@@ -1,14 +1,27 @@
 //List<Integer> = ArrayList<Integer>();
 //The <E> represents the type of array we are making
-class ArrayList <E>{
+public class ArrayList <E>{
 
+
+  //We make these variables private because we do not
+  //want them to be accessible outside of this class
+
+  //I dont want someone to do nums.objects
+
+  //We can make methods that can act as a
+  //medium for users to access these variables indirectly
+  //eg getters and setters
   private E[] objects;
   private int size;
 
 
-  private ArrayList(){
+  public ArrayList(){
     this.objects = (E[]) new Objects[];
     this.size = 0
+  }
+
+  public int getIndex(int index){
+    
   }
 
   public static void main(String[] args){
